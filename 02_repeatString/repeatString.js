85e90b335674fa43b400d6c65fa3820d5055a2a5
sami <zaman.sami8@gmail.com>
2022-11-08 00:03:45 +0600

    testing 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    repeatedText = '';
+    if (num >= 0){
+        for (num; num > 0; num--) {
+            repeatedText += string;
+        }
+        return repeatedText;
+    } else {return 'ERROR'}
+}; 
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    strArr = string.split('');
+    reverseStringArr = strArr.reverse();
+    return reverseStringArr.join('');
 };
 
 // Do not edit below this line
