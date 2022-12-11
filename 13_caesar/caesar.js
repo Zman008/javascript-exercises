4ed4134b3f24841a612c5a4f218118097883fd0d
Sami <zaman.sami8@gmail.com>
2022-12-11 20:28:06 +0600

    caesar done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -5,10 +5,34 @@ const caesar = function (text, shift) {
   for (char of textArray) {
     uni = char.charCodeAt();
     if (!/[.,?!\s]/.test(char)) {
-      uni += shift;
-    }
-    
-    enc.push(String.fromCharCode(uni));
+      nUni = uni + shift;
+      
+      condition1 = uni > 64 && uni < 91 && (nUni < 65 || nUni > 90) 
+
+      if (condition1) {
+        while (nUni > 90) {
+          nUni -= 26;
+        }
+
+        while (nUni < 65) {
+          nUni += 26;
+        }
+      }
+
+      condition2 = uni > 96 && uni < 123 && (nUni < 97 || nUni > 122) 
+
+      if (condition2) {
+        while (nUni > 122) {
+          nUni -= 26;
+        }
+
+        while (nUni < 97) {
+          nUni += 26;
+        }
+      }
+    } else {nUni = uni}
+
+    enc.push(String.fromCharCode(nUni));
   }
 
   return enc.join("");
