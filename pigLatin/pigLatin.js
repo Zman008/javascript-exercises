dde7785f6f9620583fab3780f04eb7b5e15609c6
NuranSami <zaman.sami8@gmail.com>
2022-12-29 17:39:40 +0000

    pigLatin 5 passed 4 failed

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,18 @@
-function pigLatin(string) {
+function pigLatin(text) {
+    textArray = text.split("");
+    textArrayCopy = [...textArray];
 
+    for (letter of textArray) {
+        if (['a', 'e', 'i', 'o', 'u'].includes(letter)) {
+            text = textArrayCopy.join("");
+            text += 'ay';
+            return text;
+        } else {
+            elm = textArrayCopy.shift()
+            textArrayCopy.push(elm)
+        }
+    }
 };
-  
+
 // Do not edit below this line
 module.exports = pigLatin;
