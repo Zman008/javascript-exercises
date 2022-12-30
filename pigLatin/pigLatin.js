548cc8f2700bc0b152d0edbe9082d40edf037467
NuranSami <zaman.sami8@gmail.com>
2022-12-30 06:31:21 +0000

    pigLatin done

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,18 +1,32 @@
 function pigLatin(text) {
-    textArray = text.split("");
+    wordList = text.split(' ');
+    pigL = [];
+    
+    for (word of wordList) {
+        pigL.push(toPigLatin(word));
+    }
+    
+    return pigL.join(' ');
+};
+
+function toPigLatin(word) {
+    textArray = word.split("");
     textArrayCopy = [...textArray];
 
     for (letter of textArray) {
-        if (['a', 'e', 'i', 'o', 'u'].includes(letter)) {
-            text = textArrayCopy.join("");
-            text += 'ay';
-            return text;
+        if(letter == 'u' && textArrayCopy[textArrayCopy.length - 1] == 'q') {
+            elm = textArrayCopy.shift()
+            textArrayCopy.push(elm)
+        } else if (['a', 'e', 'i', 'o', 'u'].includes(letter)) {
+            word = textArrayCopy.join("");
+            word += 'ay';
+            return word;
         } else {
             elm = textArrayCopy.shift()
             textArrayCopy.push(elm)
         }
     }
-};
+}
 
 // Do not edit below this line
 module.exports = pigLatin;
